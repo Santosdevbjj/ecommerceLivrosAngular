2249c7206221de8032e10b96230693e51f37e5b9
Sergio <santossergiorealbjj@outlook.com>
2025-10-31 19:48:09 -0300

    Update TodoContext.cs

--- Backend/Data/TodoContext.cs
+++ Backend/Data/TodoContext.cs
@@ -1,12 +1,8 @@
-// Data/TodoContext.cs
 using Microsoft.EntityFrameworkCore;
 using Backend.Models;
 
 namespace Backend.Data;
 
-/// <summary>
-/// Contexto do banco de dados usando Entity Framework.
-/// </summary>
 public class TodoContext : DbContext
 {
     public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
